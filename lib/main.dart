780ff32e62981474348bbec5d9c7766db651e18c
ji-hyun219 <ix219@hanmail.net>
2022-12-07 22:22:27 +0900

    feat(TextField): CustomTextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app/common/component/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,17 @@ class _App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(),
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          CustomTextFormField(
+            hintText: '이메일을 입력해주세요',
+            onChanged: (value) {},
+          ),
+          CustomTextFormField(
+            hintText: '비밀번호를 입력해주세요',
+            onChanged: (value) {},
+            obscureText: true,
+          ),
+        ]),
       ),
     );
   }
